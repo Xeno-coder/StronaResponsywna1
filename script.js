99058489ae4bd9e990a0489ed4f9779b232a8e9a
Xeno-coder <74659255+Xeno-coder@users.noreply.github.com>
2023-05-25 19:39:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuDisplay(){
+    var m = document.getElementById("menu")
+    if(m.style.display === "none"){
+        m.style.display = "block";
+    }else {
+        m.style.display = "none";
+    }
+}
